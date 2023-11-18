e7e01263751731b590939d51d259830cbee5191b
Kieran Pavy <jakey1947@gmail.com>
2023-11-19 02:52:44 +1000

    smol tweaks to main.cpp nothing important

--- App/main.cpp
+++ App/main.cpp
@@ -30,7 +30,7 @@ int main()
   Box box3 = Box(Vector2(0,20), Vector2(79,5) );
   Line line1 = Line(Vector2(1,21), Vector2(79, 25));
   TextBox textBox1 = TextBox(Vector2(1,1), Vector2(60,20), 
-  "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. Get ready!… Start. Or don't. I don't really care.");
+  "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
   std::vector<UIElement*> elements = 
   {
